cfb6904bb4a13d384347d23e014ef7690aa13e7d
Somasale Balaji <35003840+balajisomasale@users.noreply.github.com>
2020-06-14 14:23:12 +0530

    basic code

---
+++ certification_skills/Java/adder_calculator.java
@@ -0,0 +1,21 @@
+// Add an adder method which overrides the add method by extending the calcualtor class
+
+
+import java.util.scanner;
+
+abstract class calculator{
+abstract int add(int a,int b);
+}
+
+//Write the code here
+
+class Adder extends Calculator
+{
+int add(int a,int b){
+return a+b;
+}
+}
+
+public class Solution{
+//Takes the Input
+}
