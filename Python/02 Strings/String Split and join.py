ef0febf01f068854d1e8dc415a22a4256ed68847
Somasale Balaji <35003840+balajisomasale@users.noreply.github.com>
2022-12-25 15:00:25 -0400

    Create String Split and join.py

---
+++ Python/02 Strings/String Split and join.py
@@ -0,0 +1,14 @@
+
+def split_and_join(line):
+    # write your code here
+    l1=line.replace(" ","-")
+    return l1
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
+    
+    
+
+
