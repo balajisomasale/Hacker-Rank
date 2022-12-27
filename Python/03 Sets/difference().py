dd5274cd230f333a884bced0059cd7142e2da2ac
Somasale Balaji <35003840+balajisomasale@users.noreply.github.com>
2022-12-27 12:44:48 -0400

    Create difference().py

---
+++ Python/03 Sets/difference().py
@@ -0,0 +1,15 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+
+n=input()
+n1 = set(map(int,input().split()))
+
+b=input()
+b1=set(map(int,input().split()))
+
+print(len(n1.difference(b1)))
+
